621d79c0a041f9e71155af705809d39f548a8926
bk42327 <60442405+bk42327@users.noreply.github.com>
2020-01-29 20:04:28 +0100

    Add files via upload

--- FixSleep.py
+++ FixSleep.py
@@ -1,27 +1,22 @@
-#
-# Fix apfsd assertion for Mac power management under MacOS Catalina
+##
+# Fix apfsd assertion for Mac power management under Catalina
 # (C) 2020-01-29 Bernd Kunze
 #
 import subprocess
 import os
 import time
-import syslog
-
 
 Done = 0;
-
-syslog.syslog ('FixSleep starting');
 while  Done == 0:
     time.sleep (60);
     Result = subprocess.Popen(['pmset', '-g', 'assertions'],
                stdout=subprocess.PIPE,
                stderr=subprocess.STDOUT)
     PMSet = Result.communicate();
-    if "com.apple.apfsd.wbc_drain " in str (PMSet):
-        syslog.syslog ('FixSleep identified apfsd assertion');
+    if "wbc_drain" in str (PMSet):
         os.system ("ps -ef | grep UserEventAgent | grep -v grep | awk '{print $2}'| xargs kill -9");
-        syslog.syslog ('Fixsleep kill #1');
         time.sleep (5);
         os.system ("ps -ef | grep UserEventAgent | grep -v grep | awk '{print $2}'| xargs kill -9");
-        syslog.syslog ('Fixsleep kill #2');
+        print('\a');
+        print('\a');
         Done = 1;
