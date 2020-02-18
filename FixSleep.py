7dbe7927fee0eda32fb4d9cd35ca3034b10146b2
bk42327 <60442405+bk42327@users.noreply.github.com>
2020-02-18 13:51:55 +0100

    Add files via upload

--- FixSleep.py
+++ FixSleep.py
@@ -8,6 +8,7 @@ import time
 import sys
 import platform
 
+
 #
 # ID
 #
@@ -19,23 +20,28 @@ __version__ = "1.02"
 
 #
 OS = platform.system ()
-print (sys.argv [0], __version__)
+print (sys.argv [0] + " " + __version__)
 print (OS)
 if (OS != "Darwin"):
     printf ("Only MacOS supported.")
     exit (0)
 #
-Done = 0;
-while  Done == 0:
-    time.sleep (60);
-    Result = subprocess.Popen(['pmset', '-g', 'assertions'],
-               stdout=subprocess.PIPE,
-               stderr=subprocess.STDOUT)
-    PMSet = Result.communicate();
-    if "wbc_drain" in str (PMSet):
-        os.system ("ps -ef | grep UserEventAgent | grep -v grep | awk '{print $2}'| xargs kill -9");
-        time.sleep (5);
-        os.system ("ps -ef | grep UserEventAgent | grep -v grep | awk '{print $2}'| xargs kill -9");
-        print('\a');
-        print('\a');
-        Done = 1;
+
+def main ():
+    Done = 0;
+    while  Done == 0:
+        time.sleep (60);
+        Result = subprocess.Popen(['pmset', '-g', 'assertions'],
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.STDOUT)
+        PMSet = Result.communicate();
+        if "wbc_drain" in str (PMSet):
+            os.system ("ps -ef | grep UserEventAgent | grep -v grep | awk '{print $2}'| xargs kill -9");
+            time.sleep (5);
+            os.system ("ps -ef | grep UserEventAgent | grep -v grep | awk '{print $2}'| xargs kill -9");
+            print('\a');
+            print('\a');
+            Done = 1;
+
+if __name__ == "__main__":
+    main ()
