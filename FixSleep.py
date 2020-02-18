3cf4dd5b48b07e6916b759b9a237ba08ca9f59ab
bk42327 <60442405+bk42327@users.noreply.github.com>
2020-02-18 17:21:33 +0100

    Add files via upload

--- FixSleep.py
+++ FixSleep.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 ##
 # Fix apfsd assertion for Mac power management under Catalina
 # (C) 2020-01-29 Bernd Kunze
@@ -16,19 +17,34 @@ __author__ = "Bernd Kunze"
 __author_email__ = "bkunze@gmx.net"
 __copyright__ = "Copyright (c) 2020"
 __license__ = "BSD"
-__version__ = "1.02"
+__version__ = "1.03"
 
-#
-OS = platform.system ()
-print (sys.argv [0] + " " + __version__)
-print (OS)
-if (OS != "Darwin"):
-    printf ("Only MacOS supported.")
-    exit (0)
 #
 
 def main ():
+    OS = platform.system ()
+    print (sys.argv [0] + " " + __version__)
+#
+# We only support MacOS
+#
+    if (OS != "Darwin"):
+        print ("Only MacOS supported.")
+        exit (0)
+    if "10.15" not in str(platform.mac_ver()):
+        print ("Script supports only MacOS Catalina (10.15.x)")
+        exit (0)
+
+    print ("Waiting for assertion")
     Done = 0;
+    Eternity=20
+#
+# Logic: Run pmset command and analyze output
+# If UserEventAgent is present execute kill command twice with a sleep of 5
+# seconds, then exit.
+# If UserEventAgent is not matched, sleep 60 seconds and retry. Countdown
+# Eternity to avoid running forever: If no match after 20 minutes ve nice
+# and exit gracefully
+#
     while  Done == 0:
         time.sleep (60);
         Result = subprocess.Popen(['pmset', '-g', 'assertions'],
@@ -42,6 +58,11 @@ def main ():
             print('\a');
             print('\a');
             Done = 1;
+        print ("No match, retrying after 60 seconds");
+        Eternity = Eternity-1;
+        if Eternity == 0:
+            exit (0)
+###############################################################################
 
 if __name__ == "__main__":
     main ()
